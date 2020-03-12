f1491799c5caa65717bfedc8101a301fe1178628
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-03-13 07:56:28 +0900

    Use lex and yacc

--- onlinejudge_template/main.py
+++ onlinejudge_template/main.py
@@ -1,206 +1,246 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import argparse
-import collections
+from logging import DEBUG, INFO, basicConfig, getLogger
 from typing import *
 
 import bs4
+import ply.lex as lex
+import ply.yacc as yacc
 import requests
-import sympy
-import sympy.parsing.sympy_parser as sympy_parser
+
+logger = getLogger(__name__)
 
 
-def scrape(url: str) -> str:
-    resp = requests.get(url)
-    soup = bs4.BeautifulSoup(resp.content.decode(resp.encoding), 'html.parser')
-    if 'yukicoder.me' in url:
-        for h4 in soup.find_all('h4'):
-            if h4.string == '入力':
-                return h4.parent.find('pre').string
-    elif 'atcoder.jp' in url:
+def get_format_string(url: str, soup: bs4.BeautifulSoup) -> str:
+    if 'atcoder.jp' in url:
         for h3 in soup.find_all('h3'):
             if h3.string == '入力':
                 s = ''
                 for it in h3.parent.find('pre'):
                     s += it.string or it
                 return s
+        raise RuntimeError
+
+    elif 'yukicoder.me' in url:
+        for h4 in soup.find_all('h4'):
+            if h4.string == '入力':
+                return h4.parent.find('pre').string
+        raise RuntimeError
+
     else:
         raise NotImplementedError
-    raise RuntimeError
 
 
-def tokenize(pre: str) -> List[Any]:
-    it: List[Any] = []
-    for y, line in enumerate(pre.splitlines()):
-        line = line.replace('$', '').replace('\\(', '').replace('\\)', '')
-        line = line.replace('\\ ', ' ').replace('\\quad', ' ')
-        it += [[]]
-        for x, s in enumerate(line.split()):
-            if s in ['..', '...', '\\dots', '…', '⋯']:
-                it[-1] += [{'kind': 'dots', 'dir': ['hr', 'vr'][x == 0]}]
-            elif s in [':', '\\vdots', '⋮']:
-                it[-1] += [{'kind': 'dots', 'dir': 'vr'}]
-            elif '\\' in s:
-                assert False
-            elif '_' in s:
-                assert s.count('_') == 1
-                s, ix = s.split('_')
-                if ix.startswith('{') and ix.endswith('}'):
-                    ix = ix[1:-1]
-                if ',' in ix:
-                    raise NotImplementedError
-                it[-1] += [{'kind': 'indexed', 'name': s, 'index': ix}]
-            else:
-                it[-1] += [{'kind': 'fixed', 'name': s}]
-    return it
+tokens = (
+    'NEWLINE',
+    # 'SPACE',
+
+    # 'DOLLAR',
+    # 'VAR_OPEN',
+    # 'VAR_CLOSE',
+    'IDENT',
+    'NUMBER',
+    'UNDERSCORE',
+    'LBRACE',
+    'RBRACE',
+    'COMMA',
+    'ADD',
+    'SUB',
+    'MUL',
+    'DIV',
+    'VDOTS',
+    'DOTS',
+)
 
 
-def merge_ops(xs: List[Any]) -> List[Any]:
-    ys: List[Any] = []
-    for x in xs:
-        if ys and ys[-1]['kind'] == x['kind'] and x['kind'] in ['decl', 'decl-vector', 'read', 'read-indexed']:
-            ys[-1]['targets'] += x['targets']
-        else:
-            ys += [x]
-    return ys
+def build_lexer() -> lex.Lexer:
+    def t_NEWLINE(t: lex.LexToken) -> lex.LexToken:
+        r"""\r?\n"""
+        t.lexer.lineno += 1
+        return t
+
+    t_ignore = ' \t$'
+
+    def t_error(t: lex.LexToken) -> None:
+        raise RuntimeError("unexpected character: '{}' at line {} column {}".format(t.value, t.lineno, t.lexpos))
+
+    # t_DOLLAR = r'\$'
+    # t_VAR_OPEN = r'<\s*[vV][aA][rR]\s*>'
+    # t_VAR_CLOSE = r'<\s*/\s*[vV][aA][rR]\s*>'
+
+    t_IDENT = r'[A-Za-z]+'
+    t_NUMBER = r'[0-9]+'
+
+    t_UNDERSCORE = r'_'
+    t_LBRACE = r'{'
+    t_RBRACE = r'}'
+    t_COMMA = r','
+
+    t_ADD = r'\+'
+    t_SUB = r'-'
+    t_MUL = r'(\*|x|×|\\times)'
+    t_DIV = r'/'
+
+    t_DOTS = r'(\.\.\.*|…|\\dots|\\ldots)'
+    t_VDOTS = r'(:|⋮|\\vdots)'
+
+    return lex.lex()
 
 
-def simplify(s: str) -> str:
-    transformations = sympy_parser.standard_transformations + (sympy_parser.implicit_multiplication_application, )
-    local_dict = {'N': sympy.Symbol('N')}
-    return str(sympy_parser.parse_expr(s, local_dict=local_dict, transformations=transformations))
+class FormatNode(object):
+    line: int
+    column: int
+
+    def __init__(self, *, line: int, column: int):
+        self.line = line
+        self.column = column
+
+    def __repr__(self) -> str:
+        keys = dir(self)
+        keys = list(filter(lambda key: not key.startswith('_'), keys))
+        keys.sort()
+        keys.remove('line')
+        keys.remove('column')
+        keys.append('line')
+        keys.append('column')
+        items = ', '.join([key + '=' + repr(getattr(self, key)) for key in keys])
+        return f"{self.__class__.__name__}({items})"
 
 
-def parse(tokens: List[Any]) -> List[Any]:
-    env: Dict[str, Any] = collections.defaultdict(dict)
-    for y, line in enumerate(tokens):
-        for x, item in enumerate(line):
-            if item['kind'] == 'indexed':
-                f = env[item['name']]
-                if item['index'] in 'ijk':  # for A_1 \dots A_i \dots A_N
-                    continue
-                if 'l' not in f or item['index'] < f['l']:
-                    f['l'] = item['index']
-                if 'r' not in f or f['r'] < item['index']:
-                    f['r'] = item['index']
-    for name in env:
-        env[name]['n'] = simplify('{}-{}+1'.format(env[name]['r'], env[name]['l']))
-    it: List[Any] = []
-    used: Set[str] = set()
-    for y, line in enumerate(tokens):
-        for x, item in enumerate(line):
-            decls: List[Any] = []
-            reads: List[Any] = []
-            if item['kind'] == 'fixed':
-                decls += [{'kind': 'decl', 'names': [item['name']]}]
-                reads += [{'kind': 'read', 'names': [item['name']]}]
-            elif item['kind'] == 'indexed':
-                pass
-            elif item['kind'] == 'dots':
-                it += merge_ops(decls) + merge_ops(reads)
-                decls = []
-                reads = []
-                if item['dir'] == 'hr':
-                    assert line[x - 1]['kind'] == 'indexed'
-                    name = line[x - 1]['name']
-                    if name in used:
-                        continue
-                    n = env[name]['n']
-                    it += [{'kind': 'decl-vector', 'targets': [{'name': name, 'length': n}]}]
-                    it += [{'kind': 'loop', 'length': n, 'body': [{'kind': 'read-indexed', 'targets': [{'name': name, 'index': 0}]}]}]
-                    used.add(name)
-                elif item['dir'] == 'vr':
-                    names: List[str] = []
-                    for item in tokens[y - 1]:
-                        if item['kind'] != 'indexed':
-                            raise NotImplementedError
-                        name = item['name']
-                        if name in used:
-                            continue
-                        names += [name]
-                        used.add(name)
-                    if not names:
-                        continue
-                    acc: List[Any] = []
-                    n = env[names[0]]['n']
-                    for name in names:
-                        assert env[name]['n'] == n
-                        decls += [{'kind': 'decl-vector', 'targets': [{'name': name, 'length': n}]}]
-                        reads += [{'kind': 'read-indexed', 'targets': [{'name': name, 'index': 0}]}]
-                    it += merge_ops(decls)
-                    it += [{'kind': 'loop', 'length': n, 'body': merge_ops(reads)}]
-                    decls = []
-                    reads = []
-                else:
-                    assert False
-            else:
-                assert False
-            it += merge_ops(decls) + merge_ops(reads)
-    return it
+class SequenceNode(FormatNode):
+    items: List[FormatNode]
+
+    def __init__(self, *, items: List[FormatNode], line: int, column: int):
+        super().__init__(line=line, column=column)
+        self.items = items
 
 
-def paren_if(n: str, lr: Tuple[str, str]) -> str:
-    l, r = lr
-    if n:
-        return l + n + r
-    else:
-        return n
+class NewlineNode(FormatNode):
+    pass
 
 
-def export(it: List[Any], repeat_macro: Optional[str] = None, use_scanf: bool = False) -> str:
-    def go(it, nest):
-        if it['kind'] == 'decl':
-            if it['names']:
-                return 'int {}; '.format(', '.join(it['names']))
-        elif it['kind'] == 'decl-vector':
-            if it['targets']:
-                return 'vector<int> {}; '.format(', '.join(map(lambda x: x['name'] + paren_if(x['length'], '()'), it['targets'])))
-        elif it['kind'] in ['read', 'read-indexed']:
-            if it['kind'] == 'read':
-                items = it['names']
-            elif it['kind'] == 'read-indexed':
-                items = list(map(lambda x: x['name'] + '[' + 'ijk'[nest - x['index'] - 1] + ']', it['targets']))
-            if use_scanf:
-                return 'scanf("{}", {});\n'.format('%d' * len(items), ', '.join(map(lambda s: '&' + s, items)))
-            else:
-                return 'cin >> {};\n'.format(' >> '.join(items))
-        elif it['kind'] == 'loop':
-            s = ''
-            i = 'ijk'[nest]
-            if repeat_macro is None:
-                s += 'for (int {} = 0; {} < {}; ++ {}) '.format(i, i, it['length'], i)
-            else:
-                s += '{} ({},{}) '.format(repeat_macro, i, it['length'])
-            if len(it['body']) == 0:
-                s += ';'
-            elif len(it['body']) == 1:
-                s += go(it['body'][0], nest + 1)
-            else:
-                s += '{ '
-                for line in it['body']:
-                    s += go(line, nest + 1).rstrip() + ' '
-                s += '}\n'
-            return s
-        else:
-            assert False
+class ItemNode(FormatNode):
+    name: str
+    indices: Union[Tuple[str], Tuple]
 
-    s = ''
-    for line in it:
-        s += go(line, 0)
-    return s
+    def __init__(self, *, name: str, indices: Union[Tuple[str], Tuple] = (), line: int, column: int):
+        super().__init__(line=line, column=column)
+        self.name = name
+        self.indices = indices
+
+
+class DotsNode(FormatNode):
+    first: FormatNode
+    last: FormatNode
+
+    def __init__(self, *, first: FormatNode, last: FormatNode, line: int, column: int):
+        super().__init__(line=line, column=column)
+        self.first = first
+        self.last = last
+
+
+def build_parser(*, input: str) -> yacc.LRParser:
+    def find_column(lexpos: int) -> int:
+        line_start = input.rfind('\n', 0, lexpos) + 1
+        return lexpos - line_start + 1
+
+    def loc(p: yacc.YaccProduction) -> Dict[str, int]:
+        return {
+            'line': p.lineno(1),
+            'column': find_column(p.lexpos(1)),
+        }
+
+    def p_main(p: yacc.YaccProduction) -> None:
+        """main : lines main
+                | lines"""
+        if len(p) == 3:
+            p[0] = SequenceNode(items=[p[1]] + p[2].items, **loc(p))
+        elif len(p) == 2:
+            p[0] = SequenceNode(items=[p[1]], **loc(p))
+
+    def p_lines(p: yacc.YaccProduction) -> None:
+        """lines : line
+                 | line VDOTS newline line
+                 | line DOTS newline line"""
+        if len(p) == 2:
+            p[0] = p[1]
+        elif len(p) == 5:
+            p[0] = DotsNode(first=p[1], last=p[4], **loc(p))
+
+    def p_newline(p: yacc.YaccProduction) -> None:
+        """newline : NEWLINE"""
+        p[0] = NewlineNode(**loc(p))
+
+    def p_line(p: yacc.YaccProduction) -> None:
+        """line : items newline"""
+        p[0] = SequenceNode(items=p[1].items + [p[2]], **loc(p))
+
+    def p_items(p: yacc.YaccProduction) -> None:
+        """items : item DOTS item items
+                 | item items
+                 | item"""
+        if len(p) == 5:
+            dots = DotsNode(first=p[1], last=p[3], **loc(p))
+            p[0] = SequenceNode(items=[dots] + p[2].items, **loc(p))
+        elif len(p) == 3:
+            p[0] = SequenceNode(items=[p[1]] + p[2].items, **loc(p))
+        elif len(p) == 2:
+            p[0] = SequenceNode(items=[p[1]], **loc(p))
+
+    def p_item(p: yacc.YaccProduction) -> None:
+        """item : IDENT
+                | IDENT UNDERSCORE NUMBER
+                | IDENT UNDERSCORE IDENT
+                | IDENT UNDERSCORE LBRACE RBRACE"""
+        if len(p) == 2:
+            p[0] = ItemNode(name=p[1], indices=(), **loc(p))
+        elif len(p) == 4:
+            p[0] = ItemNode(name=p[1], indices=(p[3], ), **loc(p))
+        elif len(p) == 5:
+            raise NotImplementedError
+
+    def p_error(t: lex.LexToken) -> None:
+        raise RuntimeError("unexpected token: {} \"{}\" at line {} column {}".format(t.type, t.value, t.lineno, find_column(t.lexpos)))
+
+    return yacc.yacc(debug=False, write_tables=False)
+
+
+def run(url: str) -> None:
+    # get HTML
+    resp = requests.get(url)
+    logger.debug('HTTP response: %s', resp)
+    resp.raise_for_status()
+
+    # parse HTML
+    soup = bs4.BeautifulSoup(resp.content.decode(resp.encoding), 'html.parser')
+    logger.debug('parsed HTML: %s...', repr(str(soup))[:200])
+
+    # find the format <pre> tag
+    pre = get_format_string(url, soup)
+    pre = pre.rstrip() + '\n'
+    logger.debug('format string: %s', repr(pre))
+
+    # list tokens with lex
+    lexer = build_lexer()
+    lexer.input(pre)
+    logger.debug('Lex tokens: %s', list(lexer.clone()))
+
+    # make a tree with yacc
+    parser = build_parser(input=pre)
+    parsed = parser.parse(lexer=lexer)
+    logger.debug('Yacc tree: %s', parsed)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('url')
-    parser.add_argument('--repeat-macro')
-    parser.add_argument('--scanf', action='store_true')
+    parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
 
-    it = scrape(args.url)
-    it = tokenize(it)
-    it = parse(it)
-    print(export(it, use_scanf=args.scanf, repeat_macro=args.repeat_macro), end='')
+    if args.verbose:
+        basicConfig(level=DEBUG)
+    else:
+        basicConfig(level=INFO)
+    run(args.url)
 
 
 if __name__ == '__main__':
