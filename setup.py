e17d782bb374ed64e88efc9ed20563450b75fc4b
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-03-13 17:49:56 +0900

    v2.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='online-judge-template-generator',
-    version='2.1.0',
+    version='2.2.0',
     author='Kimiyuki Onaka',
     author_email='kimiyuki95@gmail.com',
     url='https://github.com/kmyk/online-judge-template-generator',
