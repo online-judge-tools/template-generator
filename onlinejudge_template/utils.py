257cff55d638c193a6eb104a76f13f8c7e1e94e5
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-03-19 23:54:41 +0900

    Fix

--- onlinejudge_template/utils.py
+++ onlinejudge_template/utils.py
@@ -7,7 +7,7 @@ import sympy.parsing.sympy_parser as sympy_parser
 
 def simplify(s: str, env: Set[str] = set()) -> str:
     transformations = sympy_parser.standard_transformations + (sympy_parser.implicit_multiplication_application, )
-    local_dict = {name: sympy.Symbol(name) for name in env}
+    local_dict = {name: sympy.Symbol(name) for name in (env | set(['N']))}
     expr = sympy_parser.parse_expr(s, local_dict=local_dict, transformations=transformations)
     return str(expr)
 
