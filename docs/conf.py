972056c895c4480bb8c6c074cf6d64b99f25723c
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-03-21 09:39:33 +0900

    Set up sphinx-apidoc

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,9 @@ author = 'Kimiyuki Onaka'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,9 +47,41 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Extension configuration -------------------------------------------------
+
+def run_apidoc(_):
+    ignore_paths = [
+        'setup',
+        'tests',
+        'onlinejudge_contest',
+        'onlinejudge_template_resources',
+    ]
+
+    argv = [
+        "--force",
+        "--separate",
+        "--module-first",
+        "--output-dir", ".",
+        ".."
+    ] + ignore_paths
+
+    # Sphinx 1.7+
+    from sphinx.ext import apidoc
+    apidoc.main(argv)
+
+def setup(app):
+    app.connect('builder-inited', run_apidoc)
+
+
+# -- Options for todo extension ----------------------------------------------
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
--- docs/index.rst
+++ docs/index.rst
@@ -1,15 +1,12 @@
-.. online-judge-template-generator documentation master file, created by
-   sphinx-quickstart on Sat Mar 21 09:02:49 2020.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
 Welcome to online-judge-template-generator's documentation!
 ===========================================================
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 100
    :caption: Contents:
 
+   onlinejudge_template
+   onlinejudge_random
 
 
 Indices and tables
