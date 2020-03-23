f11c63e30d81f1d55f71ca84c8dcbd55c123d0b9
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-03-24 00:02:49 +0900

    Allow absolute paths for --template option of oj-template

--- onlinejudge_template/generator/__init__.py
+++ onlinejudge_template/generator/__init__.py
@@ -18,5 +18,9 @@ def run(analyzed: AnalyzerResult, *, template_file: str) -> bytes:
         pkg_resources.resource_filename('onlinejudge_template_resources', 'template'),
     ]
     lookup = mako.lookup.TemplateLookup(directories=directories, input_encoding="utf-8", output_encoding="utf-8")
+    path = pathlib.Path(template_file)
+    if path.exists():
+        with open(path, "rb") as fh:
+            lookup.put_string(template_file, fh.read())
     template = lookup.get_template(template_file)
     return template.render(data=data)
