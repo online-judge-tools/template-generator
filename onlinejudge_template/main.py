fc3cac3ff6b3746b1f92fc72eef630b45a53bce4
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-04-05 01:05:18 +0900

    Add tests for oj-template

--- onlinejudge_template/main.py
+++ onlinejudge_template/main.py
@@ -12,22 +12,22 @@ import onlinejudge.utils
 logger = getLogger(__name__)
 
 
-def main() -> None:
+def main(args: Optional[List[str]] = None) -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('url')
     parser.add_argument('-t', '--template', default='template.cpp')
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-c', '--cookie', default=onlinejudge.utils.default_cookie_path)
-    args = parser.parse_args()
+    parsed = parser.parse_args(args=args)
 
-    if args.verbose:
+    if parsed.verbose:
         basicConfig(level=DEBUG)
     else:
         basicConfig(level=INFO)
 
     # download
-    url = args.url
-    with onlinejudge.utils.with_cookiejar(onlinejudge.utils.get_default_session(), path=args.cookie) as session:
+    url = parsed.url
+    with onlinejudge.utils.with_cookiejar(onlinejudge.utils.get_default_session(), path=parsed.cookie) as session:
         html = onlinejudge_template.network.download_html(url, session=session)
         sample_cases = onlinejudge_template.network.download_sample_cases(url, session=session)
 
@@ -36,7 +36,7 @@ def main() -> None:
     analyzed = onlinejudge_template.analyzer.combined.run(resources)
 
     # generate
-    code = onlinejudge_template.generator.run(analyzed, template_file=args.template)
+    code = onlinejudge_template.generator.run(analyzed, template_file=parsed.template)
     sys.stdout.buffer.write(code)
 
 
---
+++ tests/command_template.py
@@ -0,0 +1,57 @@
+import contextlib
+import io
+import pathlib
+import subprocess
+import sys
+import tempfile
+import textwrap
+import unittest
+from typing import *
+
+from onlinejudge_template.main import main
+
+
+class TestOJTemplateCommand(unittest.TestCase):
+    def _helper(self, *, url: str, template: str, placeholder: str, code: str, compile: Callable[[pathlib.Path], str], command: Callable[[pathlib.Path], str]):
+        with tempfile.TemporaryDirectory() as tmpdir_:
+            tmpdir = pathlib.Path(tmpdir_)
+            source_file = tmpdir / template
+            test_directory = tmpdir / 'test'
+
+            # generate
+            fh = io.TextIOWrapper(io.BytesIO(), write_through=True)
+            with contextlib.redirect_stdout(fh):
+                main(['-t', template, url])
+            code = fh.buffer.getvalue().decode().replace(placeholder, code, 1)
+            print(code)
+            with open(source_file, 'w') as fh:
+                fh.write(code)
+
+            # test
+            subprocess.check_call(compile(source_file), stdout=sys.stdout, stderr=sys.stderr)
+            subprocess.check_call(['oj', 'd', '--directory', str(test_directory), url], stdout=sys.stdout, stderr=sys.stderr)
+            subprocess.check_call(['oj', 't', '--directory', str(test_directory), '-c', command(source_file)], stdout=sys.stdout, stderr=sys.stderr)
+
+    def test_main_py_abc152_b(self) -> None:
+        url = 'https://atcoder.jp/contests/abc152/tasks/abc152_b'
+        template = 'main.py'
+        placeholder = '    # TODO: edit here\n'
+        code = textwrap.indent(textwrap.dedent("""\
+            x = str(a) * b
+            y = str(b) * a
+            return int(min(x, y))
+        """), '    ')
+        compile = lambda path: [sys.executable, '--version']
+        command = lambda path: f'{sys.executable} {str(path)}'
+        self._helper(url=url, template=template, placeholder=placeholder, code=code, compile=compile, command=command)
+
+    def test_main_cpp_aplusb(self) -> None:
+        url = 'https://judge.yosupo.jp/problem/aplusb'
+        template = 'main.cpp'
+        placeholder = '    // TODO: edit here\n'
+        code = textwrap.indent(textwrap.dedent("""\
+            return A + B;
+        """), '    ')
+        compile = lambda path: ['g++', '-std=c++14', str(path)]
+        command = lambda path: './a.out'
+        self._helper(url=url, template=template, placeholder=placeholder, code=code, compile=compile, command=command)
